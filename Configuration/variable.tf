835e11262933ea365dabd200987a9a0552e08fb4
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-08-24 20:37:49 +0100

    ajout des variables pour terra

--- Configuration/variable.tf
+++ Configuration/variable.tf
@@ -9,3 +9,25 @@ variable "location" {
   type        = string
   
 }
+
+variable "client_id" {
+  description = "Azure Service Principal Client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "client_secret" {
+  description = "Azure Service Principal Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
