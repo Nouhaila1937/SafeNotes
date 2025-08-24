5c008eb50c2900d9f754eebb9478cf54ca6d3524
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-08-24 21:51:26 +0100

    update indentation

--- Configuration/providers.tf
+++ Configuration/providers.tf
@@ -3,12 +3,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.0"
+      version = "~> 3.85"  # Version plus récente et stable
     }
   }
-  required_version = ">= 0.14.9"
+  required_version = ">= 1.0"  # Version Terraform plus récente
 }
 
 provider "azurerm" {
-  features {} 
+  features {}
+  
+  # Ces variables sont automatiquement lues depuis les env vars ARM_*
+  # ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_SUBSCRIPTION_ID, ARM_TENANT_ID
 }
