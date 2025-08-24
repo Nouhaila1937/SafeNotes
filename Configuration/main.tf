d743b13ee86258520eca8e3d4e01913efbb3fead
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-08-24 22:36:44 +0100

    always_on=false

--- Configuration/main.tf
+++ Configuration/main.tf
@@ -18,8 +18,10 @@ resource "azurerm_linux_web_app" "app_service" {
   resource_group_name   = azurerm_resource_group.rg.name
   service_plan_id       = azurerm_service_plan.app_plan.id
   depends_on            = [azurerm_service_plan.app_plan]
-  #https_only            = true
+  https_only            = true
   site_config { 
+    # pour F1 il ne faut pas toujours etre on parceque c'est gratuit F1 
+    always_on           = false
     minimum_tls_version = "1.2"
     application_stack {
       node_version = "16-lts"
