ec66d180556347af686f621d9abc1bd7c2cab49b
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-09-06 12:34:02 +0100

    suppression de firebase-admin

--- safenotes-backend/config/firebase.js
+++
@@ -1,9 +0,0 @@
-// config/firebase.js ce fichier, qui initialise Firebase Admin SDK avec ta clé privée.
-const admin = require("firebase-admin");
-const serviceAccount = require("../secrets/safenotesprivatekey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
